244796a7711a21fef0f1f3d2a35ea4db7de4854a
Zhao KK <kikakkz@hotmail.com>
2021-09-30 17:05:29 +0800

    Add banner

--- cmd/service-sample/main.go
+++ cmd/service-sample/main.go
@@ -1,8 +1,27 @@
 package main
 
 import (
-	_ "github.com/urfave/cli/v2"
+	"os"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	banner "github.com/common-nighthawk/go-figure"
+	cli "github.com/urfave/cli/v2"
 )
 
+const serviceName = "Service Sample"
+
 func main() {
+	app := &cli.App{
+		Name:  serviceName,
+		Usage: "A sample service for all npool service framework",
+		Action: func(c *cli.Context) error {
+			banner.NewColorFigure(serviceName, "", "green", true).Print()
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		logger.Sugar().Infof("fail to run %v: %v", serviceName, err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/NpoolPlatform/go-service-framework
 go 1.16
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
 	github.com/urfave/cli/v2 v2.3.0
 	go.uber.org/zap v1.19.1
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
