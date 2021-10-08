25376bbb327c1a4e89ffde6bde7e12f5985c64e6
crazyzpl <crazyzplzpl@163.com>
2021-10-08 16:22:47 +0800

    add 'config' flag

--- cmd/service-sample/main.go
+++ cmd/service-sample/main.go
@@ -27,12 +27,6 @@ func main() {
 					Usage:       "specify this service run port",
 					Destination: &port,
 				},
-				&cli.StringFlag{
-					Name:        "config",
-					Aliases:     []string{"c"},
-					Usage:       "specify the configure file",
-					Destination: &configFile,
-				},
 			},
 			Action: func(c *cli.Context) error {
 				// Here to start your service
@@ -40,11 +34,19 @@ func main() {
 			},
 		},
 	}
-	_app, err := app.NewApp(serviceName, fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n", serviceName, serviceName), "a test cli", "", nil, nil, commands)
+
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:        "config",
+			Aliases:     []string{"c"},
+			Usage:       "specify the conofig file",
+			Destination: &configFile,
+		},
+	}
+	_app, err := app.NewApp(serviceName, fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n", serviceName, serviceName), "", "", flags, nil, commands)
 	if err != nil {
 		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 	}
-
 	err = _app.Run(os.Args)
 	if err != nil {
 		logger.Sugar().Errorf("fail to run %v: %v", serviceName, err)
