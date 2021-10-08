ed2a742c27ab859dcf54846dec57eb066088d2ee
Zhao KK <kikakkz@hotmail.com>
2021-10-08 13:04:06 +0800

    Format code

--- pkg/consul/consul_test.go
+++ pkg/consul/consul_test.go
@@ -1,14 +1,34 @@
 package consul
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"testing"
+	"time"
 )
 
 func init() {
 	os.Setenv("ENV_ENVIRONMENT_TARGET", "development")
-	os.Setenv("ENV_CONSUL_HOST", "consul-server.kube-system.svc.cluster.local")
+	os.Setenv("ENV_CONSUL_HOST", "127.0.0.1")
 	os.Setenv("ENV_CONSUL_PORT", "8500")
+}
+
+func TestMain(m *testing.M) {
+	command := exec.Command("consul", "agent", "-dev")
+	go func() {
+		_, err := command.Output()
+		if err != nil {
+			fmt.Printf("local consul environment is not prepared: %v\n", err)
+		}
+	}()
+
+	time.Sleep(3 * time.Second)
+
+	exitVal := m.Run()
+
+	exec.Command("kill", "-9", fmt.Sprintf("%v", command.Process.Pid))
+	os.Exit(exitVal)
 }
 
 func TestNewConsulClient(t *testing.T) {
