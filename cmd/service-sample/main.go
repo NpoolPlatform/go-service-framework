581ebe0ad848837adf00bb320020563305e33da7
crazyzpl <crazyzplzpl@163.com>
2021-10-08 15:35:25 +0800

    add command 'start' and some relative flags

--- cmd/service-sample/main.go
+++ cmd/service-sample/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/app"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-	_ "github.com/urfave/cli/v2"
+	cli "github.com/urfave/cli/v2"
 )
 
 const serviceName = "Service Sample"
 
 func main() {
-	_app, err := app.NewApp(serviceName, "", "", "", nil, nil, nil)
+	var port uint
+	var configFile string
+
+	commands := cli.Commands{
+		&cli.Command{
+			Name:    "start",
+			Aliases: []string{"s"},
+			Usage:   "to run the app",
+			Flags: []cli.Flag{
+				&cli.UintFlag{
+					Name:        "port",
+					Aliases:     []string{"p"},
+					Usage:       "specify this service run port",
+					Destination: &port,
+				},
+				&cli.StringFlag{
+					Name:        "config",
+					Aliases:     []string{"c"},
+					Usage:       "specify the configure file",
+					Destination: &configFile,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				// Here to start your service
+				return nil
+			},
+		},
+	}
+	_app, err := app.NewApp(serviceName, fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n", serviceName, serviceName), "a test cli", "", nil, nil, commands)
 	if err != nil {
-		logger.Sugar().Infof("fail to create %v: %v", serviceName, err)
+		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 	}
 
 	err = _app.Run(os.Args)
 	if err != nil {
-		logger.Sugar().Infof("fail to run %v: %v", serviceName, err)
+		logger.Sugar().Errorf("fail to run %v: %v", serviceName, err)
 	}
 }
