3d54a948daa60cfced95aee3389a4850b6dc216a
Zhao KK <kikakkz@hotmail.com>
2021-10-08 11:05:40 +0800

    Use structure directly

--- pkg/envconf/envconf.go
+++ pkg/envconf/envconf.go
@@ -3,65 +3,44 @@ package envconf
 import (
 	"os"
 	"strconv"
+
+	"golang.org/x/xerrors"
 )
 
-type envConf struct {
-	environmentTarget string
-	consulHost        string
-	consulPort        int
-	inited            bool
+type EnvConf struct {
+	EnvironmentTarget string
+	ConsulHost        string
+	ConsulPort        int
 }
-
-var envConfInstance envConf
 
 const (
 	envValueUnknown = ""
 )
 
-func Init() {
+func NewEnvConf() (*EnvConf, error) {
 	target := os.Getenv("ENV_ENVIRONMENT_TARGET")
 	if target == envValueUnknown {
-		panic("Variable ENV_ENVIRONMENT_TARGET is not set, it must be set in environment")
+		return nil, xerrors.Errorf("Variable ENV_ENVIRONMENT_TARGET is not set, it must be set in environment")
 	}
 
 	consulHost := os.Getenv("ENV_CONSUL_HOST")
 	if consulHost == envValueUnknown {
-		panic("Variable ENV_CONSUL_HOST is not set, it must be set in environment")
+		return nil, xerrors.Errorf("Variable ENV_CONSUL_HOST is not set, it must be set in environment")
 	}
 
 	consulPortStr := os.Getenv("ENV_CONSUL_PORT")
 	if consulPortStr == envValueUnknown {
-		panic("Variable ENV_CONSUL_PORT is not set, it must be set in environment")
+		return nil, xerrors.Errorf("Variable ENV_CONSUL_PORT is not set, it must be set in environment")
 	}
 
 	consulPort, err := strconv.Atoi(consulPortStr)
 	if err != nil {
-		panic("Variable ENV_CONSUL_PORT is invalid, it must be set as int in environment")
+		return nil, xerrors.Errorf("Variable ENV_CONSUL_PORT is invalid, it must be set as int in environment")
 	}
 
-	envConfInstance.environmentTarget = target
-	envConfInstance.consulHost = consulHost
-	envConfInstance.consulPort = consulPort
-	envConfInstance.inited = true
-}
-
-func EnvironmentTarget() string {
-	if !envConfInstance.inited {
-		panic("Environment configuration is not inited, call envconf.Init() firstly")
-	}
-	return envConfInstance.environmentTarget
-}
-
-func ConsulHost() string {
-	if !envConfInstance.inited {
-		panic("Environment configuration is not inited, call envconf.Init() firstly")
-	}
-	return envConfInstance.consulHost
-}
-
-func ConsulPort() int {
-	if !envConfInstance.inited {
-		panic("Environment configuration is not inited, call envconf.Init() firstly")
-	}
-	return envConfInstance.consulPort
+	return &EnvConf{
+		EnvironmentTarget: target,
+		ConsulHost:        consulHost,
+		ConsulPort:        consulPort,
+	}, nil
 }
--- pkg/envconf/envconf_test.go
+++ pkg/envconf/envconf_test.go
@@ -9,24 +9,20 @@ func init() {
 	os.Setenv("ENV_ENVIRONMENT_TARGET", "development")
 	os.Setenv("ENV_CONSUL_HOST", "consul-server.kube-system.svc.cluster.local")
 	os.Setenv("ENV_CONSUL_PORT", "8500")
-	Init()
 }
 
-func TestEnvironmentTarget(t *testing.T) {
-	target := EnvironmentTarget()
-	if target != "development" {
-		t.Errorf("target is %v, expect development", target)
+func TestNewEnvConf(t *testing.T) {
+	conf, err := NewEnvConf()
+	if err != nil {
+		t.Errorf("fail to create env configuration: %v", err)
 	}
-}
-
-func TestConsulHostAndPort(t *testing.T) {
-	host := ConsulHost()
-	if host != "consul-server.kube-system.svc.cluster.local" {
-		t.Errorf("consul host is %v, expect consul-server.kube-system.svc.cluster.local", host)
+	if conf.EnvironmentTarget != "development" {
+		t.Errorf("target is %v, expect development", conf.EnvironmentTarget)
 	}
-
-	port := ConsulPort()
-	if port != 8500 {
-		t.Errorf("consul port is %v, expect 8500", port)
+	if conf.ConsulHost != "consul-server.kube-system.svc.cluster.local" {
+		t.Errorf("consul host is %v, expect consul-server.kube-system.svc.cluster.local", conf.ConsulHost)
+	}
+	if conf.ConsulPort != 8500 {
+		t.Errorf("consul port is %v, expect 8500", conf.ConsulPort)
 	}
 }
