0586782accd48b97aaa5fc8a333718dbf13dfd0f
Zhao KK <kikakkz@hotmail.com>
2021-10-09 12:36:26 +0800

    Pass mysql firstly

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -1,3 +1,12 @@
 package mysql
 
+import (
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+	"github.com/NpoolPlatform/go-service-framework/pkg/consul"
+)
+
 type Client struct{}
+
+func NewMysqlClient(cfg *config.Config, consulCli *consul.Client) (*Client, error) {
+	return nil, nil
+}
