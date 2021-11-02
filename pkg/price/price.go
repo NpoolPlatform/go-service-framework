342cf6e389c1b241a5b997ca9d2b29497193b473
Zhao KK <kikakkz@hotmail.com>
2021-11-02 11:26:32 +0000

    Use unit64 for price

--- pkg/price/price.go
+++ pkg/price/price.go
@@ -1,9 +1,9 @@
 package price
 
-func VisualPriceToDBPrice(price float32) int64 {
-	return int64(price * 1000000000000)
+func VisualPriceToDBPrice(price float32) uint64 {
+	return uint64(price * 1000000000000)
 }
 
-func DBPriceToVisualPrice(price int64) float32 {
+func DBPriceToVisualPrice(price uint64) float32 {
 	return float32(price / 1000000000000.0)
 }
