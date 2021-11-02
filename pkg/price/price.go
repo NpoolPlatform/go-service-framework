b687a4bf9b14a7ced3b8e10d12e18961dac6d814
Zhao KK <kikakkz@hotmail.com>
2021-11-02 12:29:01 +0000

    Better conversion

--- pkg/price/price.go
+++ pkg/price/price.go
@@ -9,11 +9,16 @@ import (
 const priceScale = 1000000000000
 
 func VisualPriceToDBPrice(price float64) uint64 {
-	return uint64(price * priceScale)
+	myPrice := decimal.NewFromFloat(price).Mul(decimal.NewFromInt(priceScale))
+	iPrice, err := strconv.ParseUint(myPrice.String(), 10, 64)
+	if err != nil {
+		return uint64(price * priceScale)
+	}
+	return iPrice
 }
 
 func DBPriceToVisualPrice(price uint64) float64 {
-	myPrice := decimal.NewFromInt(int64(price)).Div(decimal.NewFromFloat(priceScale))
+	myPrice := decimal.NewFromInt(int64(price)).Div(decimal.NewFromInt(priceScale))
 	fPrice, err := strconv.ParseFloat(myPrice.String(), 64)
 	if err != nil {
 		return float64(price)
