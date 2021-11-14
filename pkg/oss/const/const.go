fc4dcdd3b6e4283036b74ccce56365795550394e
Song Tang <tangsong0202@gmail.com>
2021-11-14 13:29:27 +0800

    add s3 const

---
+++ pkg/oss/const/const.go
@@ -0,0 +1,13 @@
+package constant
+
+const (
+	S3ServiceName = "s3.npool.top"
+	S3Region      = "33_region"
+	S3EndPoint    = "s3_endPoint"
+	S3AccessKey   = "s3_accessKey"
+	S3SecretKey   = "s3_secretKey"
+)
+
+const (
+	S3BucketForSphinxSign = "s3_bucket_for_sphinx_sign"
+)
