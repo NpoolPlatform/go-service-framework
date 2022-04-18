6ae18d69a4f549fbb4aa8f5f5fb0c3a0962fa07d
fxnt <1330672764@qq.com>
2022-04-18 14:42:07 +0800

    update dtm func

--- pkg/dtm/dtm.go
+++ pkg/dtm/dtm.go
@@ -5,8 +5,6 @@ import (
 
 	"golang.org/x/xerrors"
 
-	appusermgrconst "github.com/NpoolPlatform/appuser-manager/pkg/message/const"
-	inspireconst "github.com/NpoolPlatform/cloud-hashing-inspire/pkg/message/const"
 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 )
@@ -26,38 +24,4 @@ func GetService(serviceName ...string) (string, error) {
 		return "", xerrors.Errorf("Fail to query %v service: %v", service, err)
 	}
 	return fmt.Sprintf("%v:%v", svc.Address, svc.Port), nil
-}
-
-func GetGrpcURL(serviceName, actionGrpcName, compensateGrpcName string, service ...string) (action, compensate string, err error) {
-	grpcURL, err := SetPackageAndService(serviceName, service...)
-	if err != nil {
-		return "", "", err
-	}
-	actionGrpcURL := grpcURL + "/" + actionGrpcName
-	compensateGrpcURL := grpcURL + "/" + compensateGrpcName
-	return actionGrpcURL, compensateGrpcURL, nil
-}
-
-func SetPackageAndService(serviceName string, service ...string) (string, error) {
-	switch serviceName {
-	case appusermgrconst.ServiceName:
-		serviceName, err := GetService(serviceName)
-		if err != nil {
-			return "", err
-		}
-		if len(service) != 0 {
-			return serviceName + "/app.user.manager.v1." + service[0], nil
-		}
-		return serviceName + "/app.user.manager.v1.AppUserManager", nil
-	case inspireconst.ServiceName:
-		serviceName, err := GetService(serviceName)
-		if err != nil {
-			return "", err
-		}
-		if len(service) != 0 {
-			return serviceName + "/cloud.hashing.inspire.v1." + service[0], nil
-		}
-		return serviceName + "/cloud.hashing.inspire.v1.CloudHashingInspire", nil
-	}
-	return serviceName, nil
 }
